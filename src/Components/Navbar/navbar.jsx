9a69791d4a3dd6a17f04864509e6a698418074c7
DaveStutler <hxnguyen@ucdavis.edu>
2025-01-13 17:14:20 -0800

    push menu links

--- src/Components/Navbar/navbar.jsx
+++ src/Components/Navbar/navbar.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import logo from "../../assets/logo.svg";
 import './navbar.css';
 
 const Navbar = () => {
   return (
     <div className="navbar">
-      <h1>Hello bitches</h1>
+      <img src="https://www.docplanner.com/img/logo-default-group-en.svg?v=1" alt="Docplanner Group" />
+      <ul className="nav-links">
+        <li>Home</li>
+        <li>About Me</li>
+        <li>Porfolio</li>
+        <li>Services</li>
+        <li>Contact</li>
+      </ul>
+      <div className="nav-connect">Connect With Me</div>
     </div>
   );
 }
