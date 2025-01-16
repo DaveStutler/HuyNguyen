d5f0ea2b0d5c6f50c0d57c4517dd9cbc5cfe0886
DaveStutler <hxnguyen@ucdavis.edu>
2025-01-16 13:29:17 -0800

    body set up

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Body = () => {
+const Footer = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const Body = () => {
   )
 }
 
-export default Body
+export default Footer
