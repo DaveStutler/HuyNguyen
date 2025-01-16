c6e8ab99de8f03ff7bb7c5599aac5ad3a2c07f5a
DaveStutler <hxnguyen@ucdavis.edu>
2025-01-16 13:26:44 -0800

    layout finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
-import Navbar from './Components/Navbar/Navbar.jsx';
-import Header from './Components/Header/Header.jsx';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Layout from './pages/Layout.jsx';
+import Home from './pages/Home.jsx';
+import AboutMe from './pages/AboutMe.jsx';
+import Porfolio from './pages/Porfolio.jsx';
+import Contact from './pages/Contact.jsx';
 
 const App = () => {
   return (
     <div>
-      <Navbar />
-      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path='about' element={<AboutMe />} />
+            <Route path='porfolio' element={<Porfolio />} />
+            <Route path='contact' element={<Contact />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/Components/Navbar/navbar.jsx
+++ src/Components/Navbar/navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import logo from "../../assets/logo-transparent.png";
+import { Link } from "react-router-dom";
 import './navbar.css';
 
 const Navbar = () => {
@@ -7,11 +8,10 @@ const Navbar = () => {
     <div className="navbar">
       <img src={logo} alt="Huy Nguyen" width={150} height={100}/>
       <ul className="nav-links">
-        <li>Home</li>
-        <li>About Me</li>
-        <li>Porfolio</li>
-        <li>Services</li>
-        <li>Contact</li>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/about'>About Me</Link></li>
+        <li><Link to='/porfolio'>Porfolio</Link></li>
+        <li><Link to='/contact'>Contact</Link></li>
       </ul>
       <div className="nav-connect">Connect With Me</div>
     </div>
