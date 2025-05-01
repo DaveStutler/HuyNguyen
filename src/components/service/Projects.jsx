fac01c58f576e95f86cd348308824854ffd6ba2d
DaveStutler <hxnguyen@DaveNguyen>
2025-04-30 20:57:24 -0700

    ✨ feat new gifs for projects

---
+++ src/components/service/Projects.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import './Projects.css'
+import arrow_icon from '../../assets/arrow_icon.png'
+import theme_patttern from '../../assets/theme-patten.png'
+import { projects } from '../../assets/data/projects.json'
+
+/**
+ * @summary Service component
+ * @description This component renders the projects offered by the user.
+ * It displays the projects in a grid format with a title, description, 
+ * and a link to read more.
+ * 
+ * Provide a picture for each project as a preview of the project.
+ * Update the projects.json file to include the picture. 
+ * @returns {JSX.Element}
+ */
+
+const Projects = () => {
+    return (
+        <div className="projects">
+            <div className="project-title">
+                <h1>Projects</h1>
+                {/* You can include a header image if desired */}
+                <img src= {theme_patttern} alt="Projects Preview" />
+            </div>
+            <div className="project-container">
+                {projects.map((project) => (
+                    <div className="projects-format" key={project.id}>
+                        <img
+                            src={project.image}
+                            alt={project.name}
+                            className="project-image"
+                        />
+                        <h2>{project.name}</h2>
+                        <p>{project.description}</p>
+                        <p><strong>Status:</strong> {project.status}</p>
+                        <div className="projects-readmore">
+                            <img
+                                src={arrow_icon}
+                                alt="arrow"
+                                className="arrow-icon"
+                            />
+                            <a
+                                href={project.link}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                See More
+                            </a>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Projects
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Header from "../components/header/Header";
-import Service from "../components/service/Service";
+import Projects from "../components/service/Projects";
 import Footer from "../components/footer/Footer";
 
 const Home = () => {
   return (
     <div>
         <Header />
-        <Service />
+        <Projects />
         <Footer />
     </div>
   );
